ef3c6f3f58e3d37aa1d5693f6634a7b6243b232e
SiddheshVA <siddheshramteke@gmail.com>
2024-09-01 14:22:20 +0530

    changes in pw.java

--- pw.java
+++ pw.java
@@ -1,5 +1,31 @@
-public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello, World!");
-  }
+package loops;
+
+import java.util.Scanner;
+
+public class pattern12 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner =new Scanner(System.in);
+		System.out.print("the number is :");
+		int n=scanner.nextInt();
+		
+		for(int j=0;j<n;j++)
+		{
+			for(int i=0;i<n;i++)
+			{
+				if(i==0 || i==n-1 ||i==j && j<=(n-1)/2 || i+j==n-1 && j<=(n-1)/2)
+				{
+					System.out.print("* ");
+				}
+				else
+				{
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
 }
